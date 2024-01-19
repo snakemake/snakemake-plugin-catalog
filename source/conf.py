f9430db101a1de2f37bb7b76fa168d66daf6d915
Johannes Koester <johannes.koester@uni-due.de>
2024-01-19 17:52:38 +0100

    docs: use sphinxawesome-theme

--- source/conf.py
+++ source/conf.py
@@ -11,6 +11,7 @@ copyright = "2023, The Snakemake team"
 author = "Johannes Köster"
 
 import sys
+from sphinxawesome_theme.postprocess import Icons
 
 sys.path.insert(0, ".")
 from collect_plugins import collect_plugins
@@ -29,22 +30,20 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "lutra"
+html_theme = "sphinxawesome_theme"
 html_static_path = ["_static"]
-# html_css_files = ["theme.css"]
+html_css_files = ["custom.css"]
 html_theme_options = {
     "primary_color": "emerald",
     "secondary_color": "emerald",
-    "dark_logo": "logo-snake.svg",
-    "light_logo": "logo-snake.svg",
-    "navigation_style": "plain",
-    "sidebar_links": [
-        {
-            "text": "Snakemake documentation",
-            "alt": "Snakemake documentation",
-            "href": "https://snakemake.readthedocs.io",
-        },
-    ],
+    "logo_light": "logo-snake.svg",
+    "logo_dark": "logo-snake.svg",
+    "main_nav_links": {
+        "Snakemake homepage": "https://snakemake.github.io",
+        "Snakemake documentation": "https://snakemake.readthedocs.io",
+    },
 }
 html_title = "Snakemake plugin catalog"
-html_css_files = ["theme.css"]
+html_css_files = ["custom.css"]
+pygments_style = "sphinx"
+html_permalinks_icon = Icons.permalinks_icon
